7d02aa69f3f8a93dd158dee3f0ca5af856f8cfc7
berkesas <nazarmammedov2016@gmail.com>
2023-01-24 23:29:26 +0500

    fix commits

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,7 @@
-#define URL route for index() view
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('items/', views.MenuItemsView.as_view()),
+    path('item/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
