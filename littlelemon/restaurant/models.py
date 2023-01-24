cff877a0fe9517dccb30c352198e446abe7b503f
berkesas <nazarmammedov2016@gmail.com>
2023-01-24 21:56:42 +0500

    add models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    no_of_guests = models.PositiveIntegerField(null=False, blank=False)
+    booking_date = models.DateTimeField(auto_now=True)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField(null=False, blank=False)
